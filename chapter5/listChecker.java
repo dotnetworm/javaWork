1847871c5adf1dfc6fbb9c268da261d0ec87f9a8
Eren Soylu <ernsoylu@gmail.com>
2023-02-26 17:29:40 +0000

    new Examples

--- chapter5/listChecker.java
+++ chapter5/listChecker.java
@@ -1,15 +1,23 @@
-package chapter5;
-
 public class listChecker {
     private int[] series;
+    private int upperLimit;
 
     public void setListValues(int[] values){
         series = values;
     }
 
+    public void setUpperLimit(int limit){
+        upperLimit = limit;
+    }
     public void getListValues(){
         for(int i:series){
-            System.out.println("One of the defined values is" + String(i));
+            System.out.println("One of the defined values is " + i);
+        }
+    }
+
+    public void countUp(){
+        for(int i =0; i<=upperLimit; i++){
+            System.out.println("I am counting: "+ i);
         }
     }
 }
--- chapter5/listCheckerTestDrive.java
+++ chapter5/listCheckerTestDrive.java
@@ -1,8 +1,12 @@
-package chapter5;
-
 public class listCheckerTestDrive {
-    public static class main(String[] args){
+    public static void main(String[] args){
         listChecker checkMe = new listChecker();
-        
+        int[] checkable = {3,5,8};
+        int limitable = 15;
+        checkMe.setListValues(checkable);
+        checkMe.getListValues();
+
+        checkMe.setUpperLimit(limitable);
+        checkMe.countUp();
     }
 }
